e26f5d132b4f0b3ffd0bb6a03cde7cb2fb9fc3b7
yaocong <rogwel@vip.qq.com>
2022-07-20 19:06:46 +0800

    Q731：新题 Q1260：新题 Q19：新题 Q20：新题 Q21：旧题 Q22：新题

---
+++ src/com/hot100/Q20/Main.java
@@ -0,0 +1,69 @@
+package com.hot100.Q20;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "()"
+ * 输出：true
+ * 示例2：
+ *
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例3：
+ *
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例4：
+ *
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例5：
+ *
+ * 输入：s = "{[]}"
+ * 输出：true
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ *
+ * */
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty()){
+                if ((stack.getLast() == '(' && c == ')')
+                || (stack.getLast() == '[' && c == ']')
+                || (stack.getLast() == '{' && c == '}')
+                ){
+                    stack.removeLast();
+                    continue;
+                }
+            }
+            stack.addLast(c);
+        }
+        return stack.isEmpty();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "{[]}";
+        System.out.println(solution.isValid(s));
+    }
+}
