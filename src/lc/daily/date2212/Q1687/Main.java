c0634ffb3e856609e1181e322c020bbb27b1a186
yaocong <rogwel@vip.qq.com>
2022-12-06 21:38:27 +0800

    Q1687：新题 Q1805：新题（难度太高了，不理解，放弃）

---
+++ src/lc/daily/date2212/Q1687/Main.java
@@ -0,0 +1,63 @@
+package lc.daily.date2212.Q1687;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution {
+    public int boxDelivering(int[][] boxes, int portsCount, int maxBoxes, int maxWeight) {
+        int n = boxes.length;
+        int[] count = new int[n];
+        long[] preSumW = new long[n + 1];
+        int[] preSumCount = new int[n + 1];
+
+        for (int i = 1; i < n; i++) {
+            count[i] = boxes[i][0] == boxes[i - 1][0] ? 0 : 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            preSumW[i] = preSumW[i - 1] + boxes[i - 1][1];
+            preSumCount[i] = preSumCount[i - 1] + count[i - 1];
+        }
+
+        Deque<Integer> q = new LinkedList<>();
+        q.addLast(0); // 放入0号箱子
+
+        int[] f = new int[n + 1];
+        int[] g = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            while (i - q.getFirst() > maxBoxes || preSumW[i] - preSumW[q.getFirst()] > maxWeight){
+                q.removeFirst();
+            }
+
+            f[i] = g[q.getFirst()] + preSumCount[i] + 2;
+
+            if (i != n){
+                g[i] = f[i] - preSumCount[i+1];
+                while (!q.isEmpty() && g[i] <= g[q.getLast()]){
+                    q.removeLast();
+                }
+                q.addLast(i);
+            }
+        }
+        return f[n];
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] boxes = {
+                {2,4},
+                {2,5},
+                {3,1},
+                {3,2},
+                {3,7},
+                {3,1},
+                {4,4},
+                {1,3},
+                {5,2}
+        };
+        int portsCount = 5, maxBoxes = 5, maxWeight = 7;
+        System.out.println(solution.boxDelivering(boxes,portsCount,maxBoxes,maxWeight));
+    }
+}
---
+++ src/lc/daily/date2212/Q1805/Main.java
@@ -0,0 +1,43 @@
+package lc.daily.date2212.Q1805;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numDifferentIntegers(String word) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < word.length();) {
+            char c = word.charAt(i);
+            if (c >= '0' && c <= '9'){
+                boolean isBeginning = true; // 寻找开头
+                // 双指针
+                int start = i, end = i;
+                while (i < word.length() && word.charAt(i) >= '0' && word.charAt(i) <= '9'){
+                    char num = word.charAt(i);
+                    if (isBeginning){
+                        if (num != '0'){ // 发现第一个不是0的元素
+                            isBeginning = false;
+                            start = i;
+                            end = i;
+                        }
+                    }else{
+                        end = i;
+                    }
+                    i++;
+                }
+                set.add(word.substring(start,end + 1));
+            }else{
+                i++;
+            }
+        }
+        return set.size();
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numDifferentIntegers("035985750011523523129774573439111590559325a1554234973"));
+    }
+}
