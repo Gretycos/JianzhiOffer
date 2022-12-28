00b639a4039f28e649199ff8894e7048bed69981
yaocong <rogwel@vip.qq.com>
2022-12-28 15:16:38 +0800

    每日一题

---
+++ src/lc/daily/date2212/Q1750/Main.java
@@ -0,0 +1,21 @@
+package lc.daily.date2212.Q1750;
+
+class Solution {
+    public int minimumLength(String s) {
+        int n = s.length();
+        int l = 0, r = n - 1;
+        while (l < r && s.charAt(l) == s.charAt(r)){
+            char c = s.charAt(l);
+            while (l <= r && s.charAt(l) == c){
+                l++;
+            }
+            while (l <= r && s.charAt(r) == c){
+                r--;
+            }
+        }
+        return r - l + 1;
+    }
+}
+
+public class Main {
+}
