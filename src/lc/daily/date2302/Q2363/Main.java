f44235a4c0f7bbb5b368f8f67ce6246001756c65
yaocong <rogwel@vip.qq.com>
2023-02-28 18:22:10 +0800

    每日一题

---
+++ src/lc/daily/date2302/Q2363/Main.java
@@ -0,0 +1,27 @@
+package lc.daily.date2302.Q2363;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        int[] valueToWeight = new int[1001];
+        for (int[] item : items1) {
+            valueToWeight[item[0]] += item[1];
+        }
+        for (int[] item : items2) {
+            valueToWeight[item[0]] += item[1];
+        }
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < valueToWeight.length; i++) {
+            if (valueToWeight[i] != 0){
+                res.add(List.of(i, valueToWeight[i]));
+            }
+        }
+        return res;
+    }
+}
+
+public class Main {
+}
